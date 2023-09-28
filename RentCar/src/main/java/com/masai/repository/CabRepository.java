1ca0cad0f8bc71f81b283d6393b0df5339c80dc8
Umesh <yomyouronlinemart@gmail.com>
2023-09-28 11:39:03 +0530

    fw23_0249_day-2

---
+++ RentCar/src/main/java/com/masai/repository/CabRepository.java
@@ -0,0 +1,12 @@
+package com.masai.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import com.masai.entity.Cab;
+
+@Repository
+public interface CabRepository extends JpaRepository<Cab, Integer>{
+	
+
+}
