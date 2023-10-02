794187b0820c2c168ba833f253c0f7df4077e93d
Navneet3009 <navneetsrivastava52@gmail.com>
2023-10-02 21:27:36 +0530

    whole controller done

---
+++ RentCar/src/main/java/com/masai/Controller/CustomerController.java
@@ -0,0 +1,54 @@
+package com.masai.Controller;
+
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.masai.Exception.CurrentUserSessionException;
+import com.masai.Exception.CustomerException;
+import com.masai.Model.Customer;
+import com.masai.Service.CustomerService;
+
+@RestController
+@RequestMapping("/customer")
+public class CustomerController {
+
+	@Autowired
+	private CustomerService customerService;
+	
+	@PostMapping("/register")
+	public ResponseEntity<Customer> registerCustomer(@RequestBody Customer customer) throws CustomerException{
+		return new ResponseEntity<Customer>(customerService.insertCustomer(customer),HttpStatus.CREATED);
+	}
+	
+	@PutMapping("/update")
+	public ResponseEntity<Customer> updateCustomer(@RequestBody Customer customer, @RequestParam("uuid") String uuid) throws CustomerException, CurrentUserSessionException{
+		return new ResponseEntity<Customer>(customerService.updateCustomer(customer, uuid),HttpStatus.OK);
+	}
+	
+	@DeleteMapping("/delete")
+	public ResponseEntity<Customer> deleteCustomer(@RequestParam("customerId") Integer customerId,@RequestParam("uuid") String uuid) throws CustomerException, CurrentUserSessionException{
+		return new ResponseEntity<Customer>(customerService.deleteCustomer(customerId, uuid),HttpStatus.OK);
+	}
+	
+	@GetMapping("/viewAllCustomer")
+	public ResponseEntity<List<Customer>> viewCustomer(@RequestParam("uuid") String uuid) throws CustomerException, CurrentUserSessionException{
+		return new ResponseEntity<List<Customer>>(customerService.viewCustomer(uuid),HttpStatus.OK);
+	}
+	
+	@GetMapping("/viewCustomer")
+	public ResponseEntity<Customer> viewCustomer(@RequestParam("customerId") Integer customerId,@RequestParam("uuid") String uuid) throws CustomerException, CurrentUserSessionException{
+		return new ResponseEntity<Customer>(customerService.viewCustomer(customerId, uuid),HttpStatus.OK);
+	}
+}
---
+++ RentCar/src/main/java/com/masai/Controller/DriverController.java
@@ -0,0 +1,55 @@
+package com.masai.Controller;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.HttpStatusCode;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.masai.Exception.CurrentUserSessionException;
+import com.masai.Exception.DriverException;
+import com.masai.Model.Driver;
+import com.masai.Service.DriverService;
+
+@RestController
+@RequestMapping("/driver")
+public class DriverController {
+
+	@Autowired
+	private DriverService driverService;
+	
+	@PostMapping("/register")
+	public ResponseEntity<Driver> register(@RequestBody Driver driver) throws DriverException{
+		return new ResponseEntity<Driver>(driverService.insertDriver(driver),HttpStatus.CREATED);
+	}
+	
+	@PutMapping("/update")
+	public ResponseEntity<Driver> update(@RequestBody Driver driver, @RequestParam("uuid") String uuid) throws DriverException, CurrentUserSessionException{
+		return new ResponseEntity<Driver>(driverService.updateDriver(driver, uuid),HttpStatus.OK);
+	}
+	
+	@DeleteMapping("/delete")
+	public ResponseEntity<Driver> delete(@RequestParam("driverId") Integer driverId,@RequestParam("uuid") String uuid) throws DriverException, CurrentUserSessionException{
+		return new ResponseEntity<Driver>(driverService.deleteDriver(driverId, uuid),HttpStatus.OK);
+	}
+	
+	@GetMapping("/viewBestDriver")
+	public ResponseEntity<List<Driver>> viewBestDriver(@RequestParam("uuid") String uuid) throws DriverException, CurrentUserSessionException{
+		return new ResponseEntity<List<Driver>>(driverService.viewBestDriver(uuid),HttpStatus.OK);
+	}
+	
+	@GetMapping("viewDriver")
+	public ResponseEntity<Driver> viewDriver(@RequestParam("driverId") Integer driverId,@RequestParam("uuid")String uuid) throws DriverException, CurrentUserSessionException{
+		return new ResponseEntity<Driver>(driverService.viewDriver(driverId, uuid),HttpStatus.OK);
+	}
+}	
+
